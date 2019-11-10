eb2b64e2e046a7ad5281dd01ed23c9516b844eae
miguelbnmv <7222miguel@gmail.com>
2019-11-10 19:17:15 +0000

    Add more services, improve progress bar and more changes

--- js/step-bar.js
+++ js/step-bar.js
@@ -1,25 +1,22 @@
 let step = 'step1';
 
 function next() {
-  const step1 = document.getElementById('step1');
   const step2 = document.getElementById('step2');
   const step3 = document.getElementById('step3');
-  var desc = document.getElementById('prog_desc');
+  var desc = document.getElementById('progressDescription');
 
   if (step === 'step1') {
     step = 'step2';
-    step1.classList.add("active");
+    step2.classList.add("active");
     desc.innerHTML= "Comunicação e acompanhamento contínuos da empresa com a equipa, de forma a assegurar que o trabalho vai de encontro à realidade da empresa.";
 
   } else if (step === 'step2') {
     step = 'step3';
-    step2.classList.add("active");
     step3.classList.add("active");
     desc.innerHTML= "Finalização e entrega do projeto final.";
 
   } else if (step === 'step3') {
     step = 'step1';
-    step1.classList.remove("active");
     step2.classList.remove("active");
     step3.classList.remove("active");
     desc.innerHTML= "Análise da situação atual da empresa e identificação das suas necessidades.";
